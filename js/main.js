a45f195a4ab0baebe2556022271a3222f6b600e1
Graphic <graphic@SOD-SJH-516-04M.local>
2015-11-27 15:04:54 -0500

    character control, fire slide, bush hide

--- js/main.js
+++ js/main.js
@@ -1,6 +1,40 @@
-$('body').on('click', function(e){
-	var x = e.pageX;
-	var y = e.pageY;
+$(document).ready(function() {
 
-	console.log(x,y);
+	//Fox ready
+    $(document).keydown(function(key) {
+        switch(parseInt(key.which,10)) {
+			// Left arrow key pressed
+			case 37:
+				$('#fox').animate({left: "-=50px"}, 'fast');
+				break;
+			// Up Arrow Pressed
+			case 38:
+				$('#fox').animate({top: "-=50px"}, 'fast');
+					var dist = $('#fox').position().top;
+					console.log(dist);
+				break;
+			// Right Arrow Pressed
+			case 39:
+				$('#fox').animate({left: "+=50px"}, 'fast');
+				break;
+			// Down Arrow Pressed
+			case 40:
+				$('#fox').animate({top: "+=50px"}, 'fast');
+				break;
+		}
+	});
+    
+
+	$('.start').on('click', function(e){
+
+		$('.fire').animate({top:'-=500px'}, 10000);
+
+		$('.northBush').delay(500).fadeOut('fast');
+	});
+
+
+
 });
+
+
+
