268032b71984d231e5654c7b412c50715ccdf7c0
Graphic <graphic@SOD-SJH-516-04M.local>
2015-12-04 13:19:37 -0500

    get coordinates of character, still trying to make collision work, tried new js fun overlap

--- js/main.js
+++ js/main.js
@@ -1,8 +1,7 @@
 $(document).ready(function() {
 
 // var levels = []
-// array should be order which bushes disappear
-
+//
 
 
 	//Fox ready
@@ -11,20 +10,41 @@ $(document).ready(function() {
 			// Left arrow key pressed
 			case 37:
 				$('#fox').animate({left: "-=50px"}, 'fast');
+
+				var fposL = $('#fox').offset().left;
+					console.log(fposL);
 				break;
+
+
+
 			// Up Arrow Pressed
 			case 38:
 				$('#fox').animate({top: "-=50px"}, 'fast');
-					var dist = $('#fox').position().top;
-					console.log(dist);
+					var fposT = $('#fox').offset().top;
+					console.log(fposT);
+
+					// if (fposT <= $('#fox').width() ){
+						//stop ability to move up -- fix top position?
+						// alert('stop moving')
+					// }
+
 				break;
+
+
+
 			// Right Arrow Pressed
 			case 39:
-				$('#fox').animate({left: "+=50px"}, 'fast');
+				$('#fox').animate({right: "-=50px"}, 'fast');
+					var fposR = $('#fox').offset().left + $('.fox').width();
+					console.log(fposR);
+
 				break;
+
 			// Down Arrow Pressed
 			case 40:
 				$('#fox').animate({top: "+=50px"}, 'fast');
+					var fposB = $('#fox').offset().top + $('.fox').height();
+					console.log(fposB);
 				break;
 		}
 	});
@@ -32,18 +52,23 @@ $(document).ready(function() {
 
 	$('.start').on('click', function(e){
 
+		var firepos = $('.fire').offset().top;
+		console.log(firepos);
+
+
 		$('.fire').delay(500).animate({top:'-=500px'}, 10000);
+
 
 		$('.westBush').delay(1000).fadeOut('fast');
 		
 		$('.start').hide('fast');
 
+
+
 	});
 
-		var hitList = $('#fox').collision('.fire');
-		if (hitlist = true){
-			console.log("Gameover!");
-		}
+
+
 
 
 });
@@ -53,3 +78,9 @@ $(document).ready(function() {
 
 
 // Collision http://stackoverflow.com/questions/5419134/how-to-detect-if-two-divs-touch-with-jquery
+
+
+
+// How to make levels
+// How to stop movement
+// Collision help
